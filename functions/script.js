8e835602f11ca7af0008ea74d99edce19303571e
kosarajunavya <navya.k@volkotech.in>
2021-04-02 08:34:17 +0530

    functions practised file

--- functions/script.js
+++ functions/script.js
@@ -2,45 +2,33 @@ function showMessage() {
     console.log( 'Hello everyone!' );
 }
 showMessage();
+
 function Message() {
     let message1 = "Hello, I'm JavaScript!"; // local variable
-  
     console.log( message1 );
   }
-  
   Message(); // Hello, I'm JavaScript!
-  
-  console.log( message );
 
-
-  //
 
 let userName = 'John';
 
 function showMessage1() {
   let message2 = 'Hello, ' + userName;
-  console.log(message);
 }
-
+console.log( userName );
 showMessage1();
 
 //
 
 let user = 'John';
-
 function showMessage2() {
   user = "marry"; 
-
   let message3 = 'Hello, ' + user;
   console.log(message3);
 }
-
-console.log( userName ); 
-
+console.log( user ); 
 showMessage2();
-
-console.log( userName );
-
+console.log( user );
 
 //
 
@@ -59,3 +47,31 @@ function checkAge(age) {
   } else {
     console.log( 'Access denied' );
   }
+
+  function showPrimes(n) {
+    nextPrime: for (let i = 2; i < n; i++) {
+  
+      for (let j = 2; j < i; j++) {
+        if (i % j == 0) continue nextPrime;
+      }
+  
+      console.log( i ); // a prime
+    }
+  }
+  showPrimes(10);
+
+var x = myFunction(4, 3);   
+
+function myFunction(a, b) {
+  return a * b;             
+}
+console.log(x); 
+
+function myFunc(theObject) {
+  theObject.make = 'Toyota';
+}
+var mycar = {make: 'Honda', model: 'Accord', year: 1998};
+var x, y;
+console.log(x = mycar.make);
+myFunc(mycar);
+console.log(y = mycar.make);
