11352831ca4cfa51b24bcd5a70e6e248d54b8da8
kosarajunavya <navya.k@volkotech.in>
2021-04-08 15:14:33 +0530

    tasks completed

--- Jstasks/task5/script.js
+++ Jstasks/task5/script.js
@@ -1,54 +1,96 @@
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('.todo-input');
+const todoItemsList = document.querySelector('.todo-items');
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+let todos = [];
+
+todoForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  addTodo(todoInput.value); 
+});
+
+function addTodo(item) {
+  if (item !== '') {
+    const todo = {
+      id: Date.now(),
+      name: item,
+      completed: false
+    };
+
+    todos.push(todo);
+    addToLocalStorage(todos); 
+
+    todoInput.value = '';
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+function renderTodos(todos) {
+  todoItemsList.innerHTML = '';
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+  todos.forEach(function(item) {
+    const checked = item.completed ? 'checked': null;
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("remove");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+    const li = document.createElement('li');
+    li.setAttribute('class', 'item');
+    li.setAttribute('data-key', item.id);
+    /* <li class="item" data-key="20200708"> 
+          <input type="checkbox" class="checkbox">
+          Go to Gym
+          <button class="delete-button">X</button>
+        </li> */
+    if (item.completed === true) {
+      li.classList.add('checked');
     }
+
+    li.innerHTML = `
+      <input type="none" class="none" ${checked}>
+      ${item.name}
+      <button class="delete-button">remove</button>
+    `;
+    todoItemsList.append(li);
+  });
+
+}
+
+function addToLocalStorage(todos) {
+  localStorage.setItem('todos', JSON.stringify(todos));
+  renderTodos(todos);
+}
+
+function getFromLocalStorage() {
+  const reference = localStorage.getItem('todos');
+  if (reference) {
+    todos = JSON.parse(reference);
+    renderTodos(todos);
   }
 }
+
+function toggle(id) {
+  todos.forEach(function(item) {
+    if (item.id == id) {
+      item.completed = !item.completed;
+    }
+  });
+
+  addToLocalStorage(todos);
+}
+
+function deleteTodo(id) {
+  todos = todos.filter(function(item) {
+    return item.id != id;
+  });
+
+  addToLocalStorage(todos);
+}
+
+getFromLocalStorage();
+
+todoItemsList.addEventListener('click', function(event) {
+  if (event.target.type === 'none') {
+    toggle(event.target.parentElement.getAttribute('data-key'));
+  }
+
+  if (event.target.classList.contains('delete-button')) {
+    deleteTodo(event.target.parentElement.getAttribute('data-key'));
+  }
+});
