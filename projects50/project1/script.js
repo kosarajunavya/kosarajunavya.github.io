d5a053e81567c5c31d2c2f46e569cf2b9104f0b7
kosarajunavya <navya.k@volkotech.in>
2021-04-14 06:59:12 +0530

    first project completed

---
+++ projects50/project1/script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll('.panel')
+panels.forEach( panel => {
+    panel.addEventListener('click', () =>{
+        removeActiceClasses()
+        panel.classList.add('active');
+    })
+})
+
+function removeActiceClasses(){
+    panels.forEach( panel=> {
+        panel.classList.remove('active')
+    })
+}
