e1f72dd320a73886f45a44799c3c2811b0e0621e
kosarajunavya <navya.k@volkotech.in>
2021-04-23 10:11:57 +0530

    project5

--- projects50/project5/script.js
+++ projects50/project5/script.js
@@ -1,37 +1,40 @@
-const loadText = document.querySelector('.loading-text');
-const bg = document.querySelector('.bg');
-let load = 0;
-let int = setInterval(blurring, 30)
-function blurring() {
-    load++;
-    if (load > 99) {
-        clearInterval(int);
+// const loadText = document.querySelector('.loading-text');
+// const bg = document.querySelector('.bg');
+// let load = 0;
+// let int = setInterval(blurring, 30)
+// function blurring() {
+//     load++;
+//     if (load > 99) {
+//         clearInterval(int);
+//     }
+//     loadText.innerText = `${load}%`;
+//     loadText.style.opacity = scale(load, 0, 100, 1, 0)
+//     bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
+// }
+
+// function scale(number, inMin, inMax, outMin, outMax) {
+//     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+// }
+
+var load = 0;
+
+$(function() {
+    var int = setInterval(blurring, 30)
+    
+    function blurring() {
+        load++;
+        if (load > 99) {
+            clearInterval(int)
+        }
+        $(".loading-text").html(load + "%");
+        $(".loading-text").fadeOut(6000);
+        $(".bg").style.filter(`blur(${scale(load,0,100,30,0)}px)`);
+        
+
     }
-    loadText.innerText = `${load}%`;
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
-}
-
-function scale(number, inMin, inMax, outMin, outMax) {
-    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-}
-
-// var load = 0;
-// $(function() {
-//     function blurring() {
-//         load++;
-//         if (load > 99) {
-//             clearInterval(int)
-//         }
-//         $(".loading-text").html(load + "%");
-//         $(".loading-text").fadeOut(6000);
-//         bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
-
-
-//     }
-//     $(".bg").fadeIn(3000);
-//     const scale = (num, in_min, in_max, out_min, out_max) => {
-//         return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-//     }
-//     var int = setInterval(blurring, 30)
-// });
+    
+    const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+   
+});
