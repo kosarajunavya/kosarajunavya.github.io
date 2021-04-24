a0b8407a998a818058857b5411f47db36372d950
kosarajunavya <navya.k@volkotech.in>
2021-04-24 15:50:28 +0530

    coding challenge1 completed

--- jsudemy/coding challenges/codingchallenge1/script.js
+++ jsudemy/coding challenges/codingchallenge1/script.js
@@ -9,7 +9,7 @@ document.getElementById("btn").onclick = function (){
 function bmi(a, b) {
     // force the input as numbers *1
     var result1 = ((a*1/ (b*1)**(b*1)));
-    return result1; 
+    return parseInt(result1); 
 
 }
 
@@ -23,23 +23,26 @@ document.getElementById("btn1").onclick = function (){
 function bmi1(c, d) {
     // force the input as numbers *1
     var result2 = ((c*1/ (d*1)**(d*1)));
-    return result2;
+    return parseInt(result2);
 }
 
 
 
 
-// function large(result1,result2){
+ function large(){
+    var result1 = document.getElementById("bmi1").innerHTML;
+    var result2 = document.getElementById("bmi2").innerHTML; 
+    // console.log(parseInt(result1));
+    // console.log(parseInt(result2));
+    var data = '';
+    if(parseInt(result1) > parseInt(result2)){
+        data =  'First person has the larger BMI';
+    }
+    else{
+        data = 'Second person has the larger BMI'
+    }
+    document.getElementById('res').innerHTML = (data);
+}
 
-    
-//     if(result1>result2){
-//         return 'First person has the larger BMI';
-//     }
-//     else{
-//         return 'Second person has the larger BMI'
-//     }
-// }
-
-// document.getElementById('res').innerHTML = ((large(result1,result2)));
 
  
