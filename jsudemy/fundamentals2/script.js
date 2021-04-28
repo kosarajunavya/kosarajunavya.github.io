1c8c202dc4852f4a47a7eb7aa092c52a6a3b0b8a
kosarajunavya <navya.k@volkotech.in>
2021-04-28 07:31:22 +0530

    next section started

--- jsudemy/fundamentals2/script.js
+++ jsudemy/fundamentals2/script.js
@@ -1,1 +1,38 @@
 'use strict'
+
+// Activating Strict Mode
+
+
+let hasDriversLicense = false;
+const passTest = true;
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+// const interface = 'Audio';
+// const private = 534;
+
+///////////////////////////////////////
+
+// Functions
+
+function logger() {
+  console.log('My name is Jonas');
+}
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+const num1 = '23';
+console.log(num1);
+const num = Number('23');
+console.log(num);
