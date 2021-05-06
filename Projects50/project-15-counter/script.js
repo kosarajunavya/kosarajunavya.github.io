821ad3e1a2e6bfd602ea28aadb9af7232562a6ff
kosarajunavya <navya.k@volkotech.in>
2021-05-06 22:24:04 +0530

    counter

---
+++ Projects50/project-15-counter/script.js
@@ -0,0 +1,43 @@
+// const counters = document.querySelectorAll(".counter");
+
+// counters.forEach((counter) => {
+//   counter.innerText = "0";
+
+//   const updateCounter = () => {
+//     const target = +counter.getAttribute("data-target");
+//     const c = +counter.innerText;
+
+//     const increment = target / 200;
+
+//     if (c < target) {
+//       counter.innerText = `${Math.ceil(c + increment)}`;
+//       setTimeout(updateCounter, 1);
+//     } else {
+//       counter.innerText = target;
+//     }
+//   };
+
+//   updateCounter();
+// });
+
+$(document).ready(function () {
+  $(".counter").each(function () {
+    var $this = $(this),
+      target = $this.attr("data-target");
+
+    $({ countNum: $this.text() }).animate(
+      {
+        countNum: target,
+      },
+      {
+        increment: target / 10,
+        step: function () {
+          $this.text(Math.ceil(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum);
+        },
+      }
+    );
+  });
+});
