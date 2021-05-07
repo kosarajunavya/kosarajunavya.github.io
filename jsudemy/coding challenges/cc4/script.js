a20f975376f3413136f3d0ffafc137f4bd0d0179
kosarajunavya <navya.k@volkotech.in>
2021-05-07 22:00:57 +0530

    design for tip calculation

--- jsudemy/coding challenges/cc4/script.js
+++ jsudemy/coding challenges/cc4/script.js
@@ -1,3 +1,21 @@
-const bill = 430;
-const tip = bill <= 300 && bill >= 50 ? bill *0.15: bill*0.2;
-console.log(`the bill was ${bill}, the tip was ${tip}, and the total value is ${bill+tip}`);
+// const bill = 430;
+// const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+// console.log(
+//   `the bill was ${bill}, the tip was ${tip}, and the total value is ${
+//     bill + tip
+//   }`
+// );
+
+document.getElementById("btn").onclick = function () {
+  let bill = document.getElementById("bill").value;
+  document.getElementById("res").innerHTML = tip(bill);
+};
+function tip(bill) {
+  // force the input as numbers *1
+  if (bill) {
+    var res = +bill <= 300 && +bill >= 50 ? +bill * 0.15 : +bill * 0.2;
+    return parseFloat(res);
+  } else {
+    return `Please enter the bill value`;
+  }
+}
