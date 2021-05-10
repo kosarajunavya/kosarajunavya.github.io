a4309320551afec3ac48377b8b172baf01569e9a
kosarajunavya <navya.k@volkotech.in>
2021-05-10 07:22:25 +0530

    quotes

--- games/web-practices-master/analog-clock/clock.js
+++ games/web-practices-master/analog-clock/clock.js
@@ -1,36 +1,53 @@
-const clock = (function(){
-    const second = document.querySelector(".frame .second");
-    const minute = document.querySelector(".frame .minute");
-    const hour = document.querySelector(".frame .hour");
+const clock = (function () {
+  const second = document.querySelector(".frame .second");
+  const minute = document.querySelector(".frame .minute");
+  const hour = document.querySelector(".frame .hour");
 
-    const now = new Date();
-    let secondDeg = now.getSeconds() * 6; // every second is 6 degree
-    let minuteDeg = now.getMinutes() * 6; // every minute is 6 degree
-    let hourDeg = (now.getHours() * 30) + (minuteDeg / 60); // every hour is 30 degree + minute fraction
+  const now = new Date();
+  let secondDeg = now.getSeconds() * 6; // every second is 6 degree
+  let minuteDeg = now.getMinutes() * 6; // every minute is 6 degree
+  let hourDeg = now.getHours() * 30 + minuteDeg / 60; // every hour is 30 degree + minute fraction
 
-    // rotate arms to initial degrees
+  // rotate arms to initial degrees
+  rotate(second, secondDeg);
+  rotate(minute, minuteDeg);
+  rotate(hour, hourDeg);
+
+  tick(); // first tick
+  setInterval(tick, 1000);
+
+  function tick() {
     rotate(second, secondDeg);
-    rotate(minute, minuteDeg);
-    rotate(hour, hourDeg);
+    // increase second degree with mod 360
+    secondDeg = (secondDeg + 6) % 360;
 
-    tick(); // first tick
-    setInterval(tick, 1000);
+    if (secondDeg === 0) {
+      rotate(minute, minuteDeg);
+      rotate(hour, hourDeg);
 
-    function tick() {
-        rotate(second, secondDeg);
-        // increase second degree with mod 360
-        secondDeg = (secondDeg + 6) % 360;
-
-        if(secondDeg === 0) {
-            rotate(minute, minuteDeg);
-            rotate(hour, hourDeg);
-
-            minuteDeg = (minuteDeg + 6) % 360;
-            hourDeg = (hourDeg + (6/60)) % 360;
-        }
+      minuteDeg = (minuteDeg + 6) % 360;
+      hourDeg = (hourDeg + 6 / 60) % 360;
     }
+  }
 
-    function rotate(el, deg) {
-        el.style.transform = `rotateZ(${deg}deg)`;
-    }
+  function rotate(el, deg) {
+    el.style.transform = `rotateZ(${deg}deg)`;
+  }
 })();
+
+const url = "https://api.quotable.io/random";
+function generateQuote() {
+  fetch(url)
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (data) {
+      document.getElementById("quote").innerHTML = data.content;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+// Repeat generateQuote() every 10 seconds
+setInterval(generateQuote(), 10000);
+//Note - 10000 milliseconds = 10
