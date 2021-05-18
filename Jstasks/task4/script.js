e9fcf88167015fdc9da053a04b6eb0039077842e
kosarajunavya <navya.k@volkotech.in>
2021-05-18 20:45:34 +0530

    changes

--- Jstasks/task4/script.js
+++ Jstasks/task4/script.js
@@ -10,3 +10,16 @@ function calcAge() {
   document.getElementById("ageInSeconds").innerHTML =
     "age in seconds : " + ageInSeconds;
 }
+// $(document).ready(function () {
+//   $("#calcAge").on("click", function () {
+//     const getAge = (birthDate) =>
+//       Math.floor((new Date() - new Date(birthDate).getTime()) / 3.15576e10);
+//     let dob = $("#myDate").val(),
+//       age = getAge(dob),
+//       currentAge = parseInt(age),
+//     (ageInMinutes = age * 525600), //(365*24*60)
+//       (ageInSeconds = ageInMinutes * 60);
+//     $("#ageInMinutes").html("Age in Minutes " + currentAge);
+//     $("#ageInSeconds").html("Age in Seconds " + ageInSeconds);
+//   });
+// });
