280510f1a883d05784fc89203101b6ce87e698c3
kosarajunavya <navya.k@volkotech.in>
2021-05-21 15:59:32 +0530

    changes

--- Jstasks/task2/script.js
+++ Jstasks/task2/script.js
@@ -64,3 +64,55 @@ function propertiesOfNumber() {
   document.getElementById("negativeNumbers").innerHTML =
     "negativeNumbers are: " + negativeNumbers;
 }
+// function propertiesOfNumber() {
+//   var userInput = [];
+//   userInput=document.getElementById("num-one").value;
+//   userInput=userInput.split(',');
+//   var largest = Math.max(...userInput)
+//   var smallest = Math.min(...userInput)
+// //    var largest = Number.MIN_SAFE_INTEGER;
+// //    var smallest = Number.MAX_VALUE;
+//   var negativeNumber = [];
+//   var PositiveNumber = [];
+//   var evenNumbers = [];
+//   var odd = [];
+//   var ar = [];
+//   const array1 = [2, 3, 1];
+
+//    console.log(Math.min(...userInput));
+
+//   console.log(userInput);
+
+//   for(let i=0; i<userInput.length; i++) {
+//       if(userInput[i] > largest){
+//           largest = userInput[i];
+//       }
+//       if(userInput[i] < smallest){
+//           smallest = userInput[i];
+//       }
+
+//        if((parseInt(userInput[i]) % 2) == 0){
+//           evenNumbers.push(userInput[i]);
+
+//       }
+//        if((userInput[i] % 2) !== 0){
+//           odd.push(userInput[i]);
+//       }
+//       if(userInput[i] > 0) {
+//           PositiveNumber.push(userInput[i]);
+//       }
+//       if (userInput[i] < 0){
+//           negativeNumber.push(userInput[i]);
+//       }
+
+//   }
+//   console.log(evenNumbers);
+
+//   document.getElementById("resultArea1").innerHTML = "Largest number:" + largest;
+//       document.getElementById("resultArea2").innerHTML = "Smallest number:" + smallest;
+//       document.getElementById("resultArea3").innerHTML = "evenNumbers userInput:" + evenNumbers.toString();
+//       document.getElementById("resultArea4").innerHTML = "Odd userInput:" + odd.toString();
+//       document.getElementById("resultArea5").innerHTML = "+ve userInput:" + PositiveNumber.toString();
+//       document.getElementById("resultArea6").innerHTML = "-ve userInput:" + negativeNumber.toString();
+
+// }
