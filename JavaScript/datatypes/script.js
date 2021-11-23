89450d1bad2463f3faf3d66d21d1bb1eaaa5f208
kosarajunavya <navya.k@volkotech.in>
2021-11-23 11:35:07 +0530

    changes

--- JavaScript/datatypes/script.js
+++ JavaScript/datatypes/script.js
@@ -1,15 +1,15 @@
-var n=123;
+var n = 123;
 console.log(n);
-n=12.345;
+n = 12.345;
 console.log(n);
 
-console.log(n/0);
-console.log("not a number"/2);
+console.log(n / 0);
+console.log("not a number" / 2);
 
-const bigInt =8284752070975238797490329973457293n;
+const bigInt = 8284752070975238797490329973457293n;
 console.log(bigInt);
 
-var str = 'hello'; //single quotes
+var str = "hello"; //single quotes
 var str1 = "good morning"; //double quotes
 var str2 = `can embed another ${str1}`; // backticks
 console.log(str, str1, str2);
@@ -19,43 +19,38 @@ var name = "navya";
 console.log(`hello, ${name}`);
 
 let isGreater = 4 > 1;
-console.log( isGreater );
+console.log(isGreater);
 
-var isGreater1 = 3>5;
-console.log( isGreater1 );
+var isGreater1 = 3 > 5;
+console.log(isGreater1);
 
 var age = null;
 console.log(age);
 
 var age3 = 100;
 console.log(age3);
-age3=undefined;
+age3 = undefined;
 console.log(age3);
 
 var age1;
 console.log(age1);
 
-var age2 = '';
-console.log(age2)
+var age2 = "";
+console.log(age2);
 
-
-  
-console.log(typeof undefined);     
+console.log(typeof undefined);
 console.log(typeof 0);
 console.log(typeof 10n);
 console.log(typeof true);
 console.log(typeof "happy");
 console.log(typeof Symbol("id"));
-console.log(typeof Math );
-console.log(typeof null );
+console.log(typeof Math);
+console.log(typeof null);
 console.log(typeof alert);
 console.log(typeof console.log);
 console.log(typeof console);
 
 // console.clear(); it clears the console
 
-const myBirthday = '16.07.1998';
+const myBirthday = "16.07.1998";
 console.log(myBirthday);
-
-
-
---
+++ Jstasks/task2/main.js
@@ -0,0 +1,17 @@
+var array = [
+  [16, "navya"],
+  [18, "volkotech"],
+  [154, "css"],
+  [2, "javascript"],
+  [8, "html"],
+  [52, "jquery"],
+  [58, "drupal"],
+  [68, "electron.js"],
+  [38, "twig"],
+  [98, "php"],
+  [58, "mysql"],
+];
+
+array.sort(function (a, b) {
+  return a[0] - b[0];
+});
